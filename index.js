6a2fee1b372e863eaf5d193561a143d067f608f2
mainshum <doseit@gmail.com>
2021-09-04 15:07:37 +0200

    Almost finished mobile

--- index.js
+++ index.js
@@ -1,31 +1,21 @@
 const express = require('express');
+const {PL} = require('./text');
+
 const app = express();
 
-app.set('view engine', 'pug')
+app.set('view engine', 'ejs')
+app.use(express.static('public'));
 
-const POLISH_TEXT = {
-    MAIN_TITLE: 'Bezpesktowe',
-    P1_T1: `
-        Bezpestkowe to projekt założony
-        w 2018 roku, którego celem jest nie
-        tylko aktywne wspieranie osób z
-        zespołem Mayera-Rokitansky’egoKüstera-Hausera, ale również
-        uświadamianie i edukowanie
-        społeczeństwa. Nazwa nawiązuje do
-        pestki owocu i przyrównania jej do
-        macicy — owoce pestkowe różnią się
-        od bezpestkowych tylko
-        <strong> posiadaniem pestki, która przecież niczego nie definiuje.  </strong>
-    `,
-    P2_T1: `
-
-    `
-}
-
-app.get('/', (req, res) => {
-    res.render('mobile/index', POLISH_TEXT);
+app.get('/', function(req, res) {
+    res.render('index', {
+        BROWSER_REFRESH: process.env.BROWSER_REFRESH_URL,
+        text: PL
+    });
 });
 
 app.listen(9000, () => {
     console.log('listening')
+    if (process.send) {
+        process.send({event: 'online', url: 'http://localhost:9000/'})
+    }
 });
---
+++ text.js
@@ -0,0 +1,450 @@
+const PL = {
+  p1: {
+    mainTitle: "bezpesktowe",
+    mainText: `
+            Bezpestkowe to projekt założony
+            w 2018 roku, którego celem jest nie
+            tylko aktywne wspieranie osób z
+            zespołem Mayera-Rokitansky’egoKüstera-Hausera, ale również
+            uświadamianie i edukowanie
+            społeczeństwa. Nazwa nawiązuje do
+            pestki owocu i przyrównania jej do
+            macicy — owoce pestkowe różnią się
+            od bezpestkowych tylko
+        `,
+    textBold: `
+        posiadaniem pestki, która przecież niczego nie definiuje
+    `,
+  },
+  p2: {
+    paragraphs: [
+      `
+            W naszym społeczeństwie wciąż pokutują
+            krzywdzące stereotypy — przynależność
+            do płci często definiowana jest poprzez
+            kondycję naszych ciał. Te z nas, które
+            czują się kobietami, dobrze wiedzą, że
+            brak macicy niczego nam nie ujmuje, nie
+            sprawia, że jesteśmy ‘niepełne’. Nasza
+            kondycja jest więc konsekwentnie
+            dowodem na to, że tożsamość płciowa nie
+            ma prawa być definiowana na podstawie
+            tego, co nasze ciało ma, a czego nie.
+            `,
+      `
+            Zespół MRKH nie ujmuje bezpestkowym
+            kobiecości — najwyższy czas, by
+            wybrzmiało to głośno i wyraźnie,
+            zwłaszcza że jako bezpestkowe często
+            jesteśmy wykluczane z tzw. ‘kobiecych
+            kręgów’, w których więź często budowana
+            jest w oparciu o wspólne doświadczenia
+            związane z miesiączkowaniem i
+            macierzyństwem. Wiedząc, jakie uczucia
+            wiążą się z wykluczaniem ze wspólnot,
+            robimy wszystko, co w naszej mocy, aby
+            uniknąć podobnej atmosfery w naszej
+            społeczności.
+            `,
+      `
+            Tak jak w ogólnej populacji, tak i wśród
+            nas są osoby niebinarne oraz
+            transpłciowe, a także
+            nieheteronormatywne. Dla tej może
+            nielicznej, ale nie mniej znaczącej grupy
+            bezpestkowych docierające zewsząd
+            komunikaty z góry zakładające, że
+            wszystkie jesteśmy heteroseksualnymi
+            kobietami, prowadzą do jeszcze
+            mocniejszego poczucia wykluczenia. Z
+            szacunku do naszej różnorodności i chęci
+            tworzenia społeczności, gdzie każda/-y
+            może być sobą niezależnie od tożsamości
+            płciowej i orientacji psychoseksualnej*
+            , w
+            tej broszurze używamy naprzemiennie
+            słów ‘pacjentka’ i ‘osoba’, a ich
+            ewentualnych partnerów/-ki nazywamy
+            ‘osobami partnerskimi’
+            `,
+      `
+            Pośród nas są także osoby interpłciowe.
+            Czym jest interpłciowość? Interpłciowość
+            to pojęcie parasol dotyczące osób, które
+            rodzą się z ciałem nie wpisującym się w
+            społeczne i/lub medyczne definicje
+            typowych ciał żeńskich bądź męskich.
+            Wszystkie osoby z zespołem MRKH mogą,
+            ale nie muszą postrzegać się jako osoby
+            interpłciowe.
+            `,
+    ],
+    footer: `
+        *Tożsamość płciowa i orientacja
+        psychoseksualna to pojęcia powszechnie mylone
+        ze sobą, oznaczające w rzeczywistości dwie
+        zupełnie oddzielne rzeczy. Tożsamość płciowa to
+        osobiste poczucie przynależności do płci,
+        świadomość naszego wewnętrznego płciowego
+        ‘ja’. Z kolei orientacja psychoseksualna określa
+        nasz emocjonalny, romantyczny lub seksualny
+        pociąg do osób określonej płci.
+        `,
+  },
+  p3: {
+    header: `Czym jest zespół MRKH i z czym się wiąże?`,
+    paragraphsPt1: [
+      `
+        Zespół MRKH to uwarunkowana
+        genetycznie aplazja (niewykształcenie się
+        narządu) lub hipoplazja (znacznego
+        stopnia niedorozwój) macicy i pochwy u
+        osoby z fenotypem (przyp. red. fenotyp to
+        najogólniej wygląd) kobiecym z
+        kariotypem 46,XX. Należy do grupy
+        chorób rzadkich, ponieważ według
+        statystyk dotyczy ok. 1 na 4000 do 1 na 10
+        000 żywo urodzonych noworodków płci
+        żeńskiej (najczęściej mówi się, że jest to
+        ok. 1 na 4500 żywo urodzonych
+        noworodków płci żeńskiej). W Polsce jest
+        ok. 4400 osób z zespołem MRKH, a co roku
+        rodzi się ok. 40 bezpestkowych.
+        `,
+      `
+        Zespół MRKH u każdej osoby wygląda
+        inaczej. Od całkowitego braku macicy,
+        przez szczątkowe nieczynne rogi macicy,
+        do szczątkowych rogów macicy ze
+        szczątkową jamą z endometrium. Tak
+        samo w przypadku pochwy — może być
+        ona całkowicie nieobecna, występować w
+        szczątkowej formie lub w formie
+        zachyłka, który u niektórych
+        bezpestkowych jest na tyle duży, że przy
+        podejmowaniu prób współżycia staje się w
+        pełni funkcjonalną pochwą bez
+        interwencji medycznej.
+        `,
+      `
+        Zespół MRKH jest wadą złożoną — w
+        medycynie każda choroba, która
+        nazywana jest zespołem, składa się z
+        wielu elementów. Wadom macicy i
+        pochwy mogą towarzyszyć więc wady
+        innych narządów. Dwa najczęściej
+        dotknięte wadami układy to układ
+        moczowy oraz kostny; w niektórych
+        przypadkach pojawiają się także wady
+        serca i dużych naczyń, a część pacjentek
+        ma niedosłuch.
+        `,
+      `
+        W związku z powyższym wyróżniamy dwa
+        typu zespołu MRKH: pierwszy rozpoznaje
+        się wówczas, gdy wada ogranicza się do
+        układu rozrodczego, czyli macicy i
+        pochwy, a drugi gdy współistnieją inne
+        wady, np. wrodzony brak nerki i/lub wady
+        kręgosłupa.
+        `,
+      `
+        Choć bezpestkowe nie miesiączkują, nie
+        jest to równoznaczne z brakiem cyklu
+        miesiączkowego. Osoby z zespołem MRKH
+        mają funkcjonujące jajniki, które
+        wydzielają hormony i produkują komórki
+        jajowe, w związku z czym bezpestkowe
+        owulują i, z hormonalnego punktu
+        widzenia, przechodzą przez wszystkie
+        etapy cyklu (jak pestkowe). Jedyną
+        różnicą jest brak krwawienia, choć w
+        przypadku występowania szczątkowego
+        endometrium może dochodzić do
+        “mikromiesiączkowania”. Bezpestkowe
+        także przejdą menopauzę i mogą
+        doświadczać wielu związanych z nią
+        niedogodności.
+        `,
+      `
+        Bycie bezpestkową nie jest równoznaczne
+        z niemożnością uprawiania seksu. Każda
+        osoba definiuje seks na swój sposób, a
+        penetracja nie jest konieczna. Jeśli jednak
+        zdecydujesz się na seks penetracyjny,
+        istnieją trzy sposoby na wytworzenie
+        pochwy. Pamiętaj, żeby przed
+        rozpoczęciem działań w tym kierunku
+        skontaktować się z lekarką/lekarzem
+        prowadzącą/-ym. Którąkolwiek z opcji
+        wybierzesz, udany i satysfakcjonujący
+        seks pomimo zespołu MRKH jest możliwy.
+        Jego jakość nie zależy od tego, czy Twoja
+        pochwa ma 4 czy 7 centymetrów, ale od
+        tego, z kim decydujesz się współżyć.
+        `,
+    ],
+    belowDrop1: `
+        Pierwszą możliwością jest rozciągnięcie
+        pochwy z osobą partnerską — jeśli masz
+        kogoś, z kim będziesz czuła się na tyle
+        komfortowo, aby to zrobić (w atmosferze
+        czułości i wzajemnego zaufania),
+        rozważcie tę opcję. Może sprawić Wam
+        wiele przyjemności.
+    `,
+    belowDrop2: `
+        Drugą jest nieoperacyjne wymodelowanie
+        pochwy za pomocą dilatorów. Polega ona
+        na regularnym stopniowym rozpychaniu
+        istniejącego zachyłka pochwy. Ta opcja
+        wymaga cierpliwości i determinacji,
+        jednak daje wiele satysfakcji. Ćwiczenia
+        możesz dostosowywać do swoich potrzeb.
+        Pamiętaj, że robisz to dla siebie, więc cały
+        proces powinien sprawiać Ci przyjemność.
+    `,
+    belowDrop3: `
+        Trzecia opcja to zabieg operacyjny.
+    `,
+  },
+  p4: {
+    header: `Jak rozmawiać o zespole MRKH`,
+    paragraphs: [
+      `
+        Ważna jest weryfikacja osób, z którymi
+        dzielimy się naszymi doświadczeniami
+        bycia bezpestkową, ponieważ są osoby,
+        które mogą tego nie zrozumieć i/lub
+        zareagować w negatywny sposób.
+        Pamiętaj, że najważniejszy jest Twój
+        komfort i bezpieczeństwo, i że to Ty
+        wyznaczasz granice.
+        `,
+      `
+        Jeśli potrzebujesz porozmawiać z kimś o
+        zespole MRKH, a nie wiesz jak zacząć,
+        polecamy powtarzanie w głowie
+        wybranych kwestii, które chciałabyś
+        omówić, żeby w realnej sytuacji szło Ci
+        płynnie, a wypowiadane słowa nie
+        brzmiały obco. Ty decydujesz, kto to
+        będzie: bliska Ci osoba, druga
+        bezpestkowa, specjalista ds. zdrowia
+        psychicznego (seksulog/-ka, psycholog/-
+        ka, psychoterapeuta/-ka). Możesz sięgnąć
+        po różne formy wsparcia odpowiadające
+        Twoim potrzebom — poszukaj osób
+        wspierających w najbliższym otoczeniu,
+        zastanów się nad dołączeniem do
+        dedykowanej grupy wsparcia i/lub
+        skorzystaj ze wsparcia psychologicznego
+        lub psychoterapii.
+        `,
+      `
+        Język ma znaczenie! Ponieważ jako
+        społeczeństwo nie jesteśmy otwarci na
+        różnorodność ani na różnorodny rozwój
+        płciowy, a MRKH jest jednym z wielu
+        przypadków tej różnorodności, istotna
+        jest adekwatna narracja. Jeśli o zespole
+        MRKH mówimy otwarcie, bez
+        nacechowania i stygmatyzacji,
+        pokazujemy, że posiadanie zespołu MRKH
+        nie jest niczym złym i/lub przerażającym.
+        Nie świadczy o ‘wybrakowaniu’ i nie
+        stanowi o czyjejkolwiek wartości. Staramy
+        się unikać określeń: “cierpi”, “choruje” lub
+        “zdiagnozowano zespół MRKH”. Zamiast
+        tego mówimy, że zespół MRKH się po
+        prostu ma.
+        `,
+      `
+        Najważniejszą kwestią sojusznictwa (tj.
+        bycia osobą wspierającą) jest empatia,
+        słuchanie drugiej osoby i wyczucie (lub
+        zapytanie wprost), czy ona w ogóle chce
+        rozmawiać na ten temat — nie każda
+        osoba z zespołem MRKH chce o nim
+        mówić i nie każda musi być edukatorką i
+        aktywistką. Dlatego też bazę stanowi
+        zadanie pytania (któremu powinien
+        towarzyszyć komunikat o możliwości
+        wycofania się z rozmowy na warunkach
+        bezpestkowej): “Czy chciałabyś o tym
+        porozmawiać i czy czujesz się z tym
+        komfortowo?”. Ważne jest dawanie
+        przestrzeni, nie narzucanie się z
+        nadmierną ilością pytań oraz
+        respektowanie wyznaczonych granic.
+        Osoby z zespołem MRKH mają prawo być
+        zmęczone wypytywaniem, nie chcieć
+        odpowiadać na pytania i zachować bycie
+        bezpestkową tylko dla siebie.
+        `,
+      `
+        Niezwykle ważnym aspektem jest sposób
+        przekazywania informacji na temat
+        zespołu MRKH przez lekarza/-kę
+        prowadzącego/-cą. Nie można powiedzieć
+        pacjentce w pośpiechu, że nie będzie
+        miesiączkowała, nie może współżyć, nie
+        może mieć dzieci, dlatego że to…
+        nieprawda. Niestety nie tylko jako
+        bezpestkowe, ale w ogóle jako ludzie
+        najczęściej zapamiętujemy właśnie to, co
+        powiedziano nam za pierwszym razem.
+        Dlatego już w momencie rozpoznawania
+        zespołu MRKH należy przedstawiać
+        alternatywne rozwiązania i nie traktować
+        bycia bezpestkową jako coś złego. Ważna
+        jest rozmowa, a nie mechaniczne
+        podejmowanie działań, by pacjentka
+        miała kontrolę nad sytuacją i była
+        świadoma podejmowanych decyzji, które
+        dotyczą jej ciała.
+        `,
+    ],
+  },
+  p5: {
+    header: `Relacje i macierzyństwo`,
+    paragraphs: [
+      `
+        Proces dojrzewania osób z zespołem
+        MRKH (do momentu rozpoznania)
+        przebiega w ten sam sposób, jak u osób
+        pestkowych, wobec czego bezpestkowe
+        zazwyczaj przyjmują kobiece role płciowe
+        (ze względu na swój wygląd zewnętrzny, a
+        nie zespół MRKH).
+        Rolą terapeutów/-ek jest również
+        wsparcie osób z zespołem MRKH w
+        podejmowaniu kontaktów seksualnych,
+        lub wspólne rozpoznanie powodu, dla
+        którego ich nie podejmują. Rozmowa o
+        blokadach i szukanie rozwiązań służy
+        osiągnięciu satysfakcji w sferze
+        seksualnej.
+        `,
+      `
+        Każda osoba, niezależnie od posiadania
+        lub nieposiadania pestki, jest zbudowana
+        w inny sposób — nasze ciała, ich wygląd i
+        funkcjonalność różnią się od siebie. Jeśli
+        obawiasz się, że partner/-ka seksualny/-a
+        rozpozna, że Twoja pochwa została
+        wytworzona, zapewniamy, że nie ma
+        powodów do zmartwień — z laickiego
+        punktu widzenia nie jesteśmy w stanie
+        rozpoznać, czy mamy kontakt seksualny z
+        osobą, która urodziła się z pochwą, czy z
+        osobą, która ma neo-waginę.
+        Kolejną konsekwencją związaną z byciem
+        bezpestkową jest niemożność zajścia w
+        ciążę i urodzenia dziecka. Zazwyczaj w
+        momencie rozpoznania zespołu MRKH ta
+        informacja jest trudniejsza do przyjęcia
+        przez rodziców/opiekunów osób
+        bezpestkowych. Oczywiście posiadanie
+        zespołu MRKH nie wyklucza zostania
+        rodzicem (oczywiście o ile tego w ogóle
+        chcemy). Istnieją na to różne sposoby:
+        `,
+    ],
+    belowDrop1: `
+        Pierwszą możliwością jest transplantacja
+        macicy (przyp. red. przeszczep), która
+        wciąż jest uznawana za leczenie
+        eksperymentalne i ze względów
+        anatomicznych nie może być
+        przeprowadzona u wszystkich pacjentek z
+        zespołem MRKH. Aktualnie na świecie jest
+        ponad dziesięcioro dzieci urodzonych
+        przez pacjentki z zespołem MRKH. Do tej
+        pory najbardziej znane precedensy to te z
+        2014 i 2017 roku, kiedy kolejno w Szwecji i
+        Brazylii pacjentki z zespołem MRKH
+        urodziły żywe dzieci — w pierwszym
+        przypadku z macicy przeszczepionej od
+        żyjącej dawczyni, w drugim od zmarłej.
+        Macica nie jest narządem niezbędnym do
+        życia i przeszczepia się ją tylko i
+        wyłącznie na potrzeby jednej ciąży (po
+        porodzie zostaje ona usunięta). Choć
+        operacja jest bardzo skomplikowana i
+        obciążająca zarówno dla dawczyni, jak i
+        dla biorczyni, jest możliwa.
+    `,
+    belowDrop2: `
+        Drugą możliwością jest surogacja, która
+        polega na zapłodnieniu pozamacicznym z
+        transferem zarodka do matki zastępczej
+        (przyp. red. surogatki). W ten sposób
+        pacjentka z zespołem MRKH może być
+        genetycznym rodzicem, ponieważ z
+        punktu widzenia jajnikowego osoby z
+        zespołem MRKH są płodne.
+    `,
+    belowDrop3: `
+        Trzecią (i obecnie jedyną dostępną w
+        Polsce z punktu prawnego) możliwością
+        jest adopcja, jednak w celu rozpoczęcia
+        procedury adopcyjnej należy być w co
+        najmniej czteroletnim związku
+        małżeńskim.
+    `,
+    lastParagraph: `
+        Warto podkreślić, że są osoby, które
+        realizują potrzebę rodzicielstwa w inny
+        sposób, np. związując się z osobą, która
+        ma dziecko/dzieci, angażując się jako
+        opiekunka, bliska przyjaciółka rodziny
+        itd.
+    `,
+  },
+  p6: {
+    header: "Lista gabinetów lekarskich",
+    dOffice: `
+        Fusce sit amet luctus elit. Vivamus congue
+        lectus lectus, a mollis urna posuere quis.
+        Mauris facilisis vulputate aliquam.
+        Integer lorem tortor, fermentum sit amet
+        ipsum eget, dictum ornare leo. Curabitur
+        ornare ligula id diam tincidunt posuere.
+        Mauris ac dapibus ante. Lorem ipsum
+        dolor sit amet, consectetur adipiscing elit.
+        Nam in urna luctus, egestas sapien a,
+      `,
+    offices: [
+      `Białystok`,
+      `Bytom`,
+      `Chorzów`,
+      `Chwaszczyno`,
+      `Gdańsk`,
+      `Katowice`,
+      `Koluszki`,
+      `Kraków`,
+      `Lublin`,
+      `Łódź`,
+      `Poznań`,
+      `Rzeszów`,
+      `Ruda Śląska`,
+      `Skoczów`,
+      `Sosnowiec`,
+      `Warszawa`,
+      `Wrocław`,
+    ],
+  },
+  p7: {
+    header: `Kontakt`,
+    support: `Wesprzyj nas na Zrzutka.pl`,
+  },
+  infoKeys: {
+    phone: "Telefon",
+  },
+  infoVals: {
+    email: "bezpestkowe@gmail.com",
+  },
+};
+
+module.exports = { PL };
