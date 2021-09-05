879cb5c88f47ecb045b344f75d4be4750facf561
mainshum <doseit@gmail.com>
2021-09-05 19:44:28 +0200

    Simplest build script

--- build.js
+++ build.js
@@ -2,10 +2,16 @@ const fs = require("fs");
 const ejs = require("ejs");
 const { PL } = require("./text");
 
+fs.readdir(__dirname + "/public", (err, files) => {
+  for (const f of files)
+    fs.copyFileSync(__dirname + "/public/" + f, __dirname + "/dist/" + f);
+});
+
 ejs.renderFile(
   __dirname + "/views/index.ejs",
   { text: PL, BROWSER_REFRESH: false },
   (err, data) => {
-    fs.writeFileSync(__dirname + "/build/index.html", data);
+    fs.writeFileSync(__dirname + "/dist/index.html", data);
+    console.log(err, data);
   }
 );
