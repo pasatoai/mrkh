abf547c34613346c1f8826d4510ff882d7ef8c35
mainshum <doseit@gmail.com>
2021-09-05 19:47:18 +0200

    Make dist folder

--- build.js
+++ build.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const ejs = require("ejs");
 const { PL } = require("./text");
 
+fs.mkdirSync(__dirname + "/dist");
+
 fs.readdir(__dirname + "/public", (err, files) => {
   for (const f of files)
     fs.copyFileSync(__dirname + "/public/" + f, __dirname + "/dist/" + f);
